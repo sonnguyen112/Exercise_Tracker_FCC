8d647e258a208b7f63aecdf0627b69111d510c2f
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2022-10-07 11:54:10 +0700

    fix bug

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -7,16 +7,10 @@ class ApiController{
     async users(req, res){
         if (req.method === "POST"){
             var username = req.body["username"]
-            var user = await user_db.find({username:username})
-            if (user.length===0){ //User not exist
-                var new_user = await user_db.create([{
-                    username : username,
-                }])
-                var id = new_user[0]["_id"]
-            }
-            else{ //user is exist
-                var id = user[0]["_id"]
-            }
+            var new_user = await user_db.create([{
+                username : username,
+            }])
+            var id = new_user[0]["_id"]
             return res.json({
                 username:username,
                 _id : id
@@ -32,14 +26,32 @@ class ApiController{
         if (req.method==="POST"){
             var id_user = req.params["_id"]
             var description = req.body["description"]
-            var duration = req.body["duration"]
+            var duration = parseInt(req.body["duration"])
             var date_input = req.body["date"]
 
-            var user = await user_db.findById(id_user)
+            if (!id_user || !description || !duration){
+                return res.json({
+                    error: "Missing field"
+                })
+            }
+            try {
+                var user = await user_db.findById(id_user)
+            } catch (error) {
+                return res.json({
+                    error:error
+                })
+            }
+            if (!user){
+                return res.json({
+                    error: "User not exist"
+                })
+            }
             var username = user["username"]
-            var date = new Date(date_input)
-            if (date_input.length === 0){
-                date = new Date()
+            if (!date_input){
+                var date = new Date()
+            }
+            else{
+                var date = new Date(date_input)
             }
             await exercise_db.create({
                 id_user:id_user,
@@ -49,11 +61,11 @@ class ApiController{
             })
         
             return res.json({
-                _id:id_user,
                 username : username,
+                description: description,
+                duration: duration,
                 date: date.toDateString(),
-                duration: parseInt(duration),
-                description: description
+                _id:id_user
             })
         }
         return res.json({
@@ -65,21 +77,24 @@ class ApiController{
         var id_user = req.params["_id"]
         var from = req.query["from"]
         var to = req.query["to"]
-        var limit = req.query["limit"]
+        var limit = parseInt(req.query["limit"])
 
         var user = await user_db.findById(id_user)
         var username = user["username"]
 
-        var log_list = await exercise_db.find({id_user:id_user}).limit(limit)
+        var log_list = await exercise_db.find({id_user:id_user})
         if (from){
             log_list = log_list.filter(function(item){
-                return item["date"] >= new Date(from)
+                return item["date"] > new Date(from)
             })
         }
         if (to){
             log_list = log_list.filter(function(item){
-                return item["date"] <= new Date(to)
+                return item["date"] < new Date(to)
             })
+        }
+        if (limit){
+            log_list = log_list.slice(0, limit)
         }
         log_list = log_list.map(function(item){
             return {
